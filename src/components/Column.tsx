6a032e1920545baa60160501289c2574e83c8dc5
Bryan P. Reyes <bryanpr17@gmail.com>
2023-08-08 20:26:01 +0800

    Styling columns

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -3,7 +3,16 @@ interface ColumnProps {
 }
 
 const Column: React.FC<ColumnProps> = ({ state }) => {
-  return <h1>Column - {state}</h1>;
+  return (
+    <div className="w-80 bg-slate-600 h-96 rounded-md relative overflow-hidden">
+      <div className="absolute h-10 w-full bg-blue-600 top-0 flex items-center p-3">
+        {state}
+      </div>
+      <div className="p-3 bg-stone-50 h-full">
+        <div className="bg-red-500 w-full h-60"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Column;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -23,7 +23,7 @@ const MainBoard = () => {
             </svg>
           </div>
           <p className="text-gray-500">this is some text here</p>
-          <button className="mt-5">
+          <button className="mt-5 bg-blue-500 text-slate-50 px-4 py-2 rounded-md">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -41,9 +41,39 @@ const MainBoard = () => {
             Add Task
           </button>
         </div>
-        <div>icon here (day|night)</div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 dark:block"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 dark:hidden"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            />
+          </svg>
+        </div>
       </div>
-      <div>
+      <div className="p-5 flex justify-between">
         <Column state="TODO" />
         <Column state="INPROGRESS" />
         <Column state="DONE" />
