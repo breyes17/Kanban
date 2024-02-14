6af1c81a3f3e8dd90736fb34773b9d2b2426c39d
Bryan Reyes <bryan.reyes@fidx.co.jp>
2024-02-14 15:28:59 +0800

    update header

--- src/components/column/column-content.tsx
+++ src/components/column/column-content.tsx
@@ -1,5 +1,8 @@
+import ColumnCard from "./column-card";
+
 function ColumnContent() {
-  return <div>ColumnContent</div>;
+  const arr = Array.from(Array(5).keys());
+  return arr.map((_, i) => <ColumnCard key={i} />);
 }
 
 export default ColumnContent;
