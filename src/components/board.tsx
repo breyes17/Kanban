2948f09e513f282b6833206778752fdf4bdebcb1
Bryan Reyes <bryan.reyes@fidx.co.jp>
2024-02-27 15:49:45 +0800

    partial fix

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -5,11 +5,18 @@ const Board = () => {
   const kanban = useStore((state) => state.currentKanban);
 
   return (
-    <div className="bg-slate-100 grow p-4">
-      <div className="flex gap-5 overflow-x-scroll">
-        {kanban.boards.map((board) => (
-          <Column column={board} key={board.boardId} />
-        ))}
+    <div className="bg-slate-100 flex flex-col">
+      <div className="flex-grow">
+        <ol className="inset-0 overflow-x-auto overflow-y-hidden flex">
+          {kanban.boards.map((board) => (
+            <li
+              key={board.boardId}
+              className="self-start shrink-0 block h-full whitespace-nowrap px-2"
+            >
+              <Column column={board} />
+            </li>
+          ))}
+        </ol>
       </div>
     </div>
   );
